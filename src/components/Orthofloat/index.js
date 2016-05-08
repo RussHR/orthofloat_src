ce90a5024a7c04e2f911ee4c3f2684a5a4f6b3b9
RussHR <russ.rinzler@gmail.com>
2016-05-08 00:29:32 -0700

    animates a rotating cube

--- src/components/Orthofloat/index.js
+++ src/components/Orthofloat/index.js
@@ -3,31 +3,46 @@ import THREE from 'three';
 
 export default class Orthofloat extends Component {
     componentDidMount() {
-        const scene = new THREE.Scene();
-        const camera = new THREE.OrthographicCamera(window.innerWidth / - 16, window.innerWidth / 16, window.innerHeight / 16, window.innerHeight / - 16, -200, 500);
-        camera.position.x = 120;
-        camera.position.z = 120;
-        camera.lookAt(scene.position);
+        this.initializeScene();
+    }
 
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
-        renderer.setSize(window.innerWidth, window.innerHeight);
+    initializeScene() {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.OrthographicCamera(window.innerWidth / - 16, window.innerWidth / 16, window.innerHeight / 16, window.innerHeight / - 16, -200, 500);
+        this.camera.position.x = 120;
+        this.camera.position.z = 120;
+        this.camera.lookAt(this.scene.position);
 
-        const cubeGeometry = new THREE.CubeGeometry(4,4,4);
-        const cubeMaterial = new THREE.MeshLambertMaterial({color: 0x00ee22});
-        const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        this.cubeGeometry = new THREE.CubeGeometry(4,4,4);
+        this.cubeMaterial = new THREE.MeshLambertMaterial({color: 0x00ee22});
+        this.cube = new THREE.Mesh(this.cubeGeometry, this.cubeMaterial);
+        this.scene.add(this.cube);
 
-        scene.add(cube);
+        this.directionalLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
+        this.directionalLight.position.set(-20, 40, 60);
+        this.scene.add(this.directionalLight);
 
-        const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.7 );
-        directionalLight.position.set( -20, 40, 60 );
-        scene.add(directionalLight);
+        this.ambientLight = new THREE.AmbientLight(0x292929);
+        this.scene.add(this.ambientLight);
 
-        const ambientLight = new THREE.AmbientLight(0x292929);
-        scene.add(ambientLight);
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-        this.el.appendChild(renderer.domElement);
-        renderer.render(scene, camera);
+        this.el.appendChild(this.renderer.domElement);
+        this.renderer.render(this.scene, this.camera);
+
+        this.renderAnimation();
+    }
+
+    renderAnimation() {
+        // rotate the cube around its axes
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
+        this.cube.rotation.z += 0.01;
+
+        requestAnimationFrame(() => this.renderAnimation());
+        this.renderer.render(this.scene, this.camera);
     }
 
     render() {
