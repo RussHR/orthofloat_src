1ea7be8f0da567316a777ed7805bb62be2028248
RussHR <russ.rinzler@gmail.com>
2016-05-09 22:11:58 -0700

    menu opens and closes

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,12 +1,23 @@
 import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
 
 import './menu.scss';
 
 export default class Menu extends Component {
     render() {
+        const { isOpen, onClick } = this.props;
+        const menuClassNames = classNames({ 'is-open': isOpen });
         return (
-            <div id="menu" onClick={this.props.onClick}>
-                menu
+            <div id="menu" className={menuClassNames}>
+                <div className="menu-open" onClick={onClick}>
+                    menu
+                </div>
+                <div className="menu-contents">
+                    <div className="menu-close" onClick={onClick}>
+                        X
+                    </div>
+                    orthofloat
+                </div>
             </div>
         );
     }
