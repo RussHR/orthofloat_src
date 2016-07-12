0a49a4726c00eb7d21405c9944d501be1f4a0cde
RussHR <russ.rinzler@gmail.com>
2016-07-11 21:58:34 -0700

    change Menu from stateless function to react component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import classNames from 'classnames';
 
 import ColorInput from '../ColorInput';
@@ -6,82 +6,84 @@ import { getNewCameraAngle, randomRGB } from '../../businessLogic/threeHelpers';
 
 import './menu.scss';
 
-export default function Menu(props) {
-    const { isOpen,
-            onClickToggleMenu,
-            onChangeColor,
-            onClickToggleStats,
-            cameraAngle,
-            onChangeCameraAngle,
-            topColor,
-            bottomColor,
-            isMobile } = props;
-    const menuClassNames = classNames({ 'is-open': isOpen });
+export default class Menu extends Component {
+    render() {
+        const { isOpen,
+                onClickToggleMenu,
+                onChangeColor,
+                onClickToggleStats,
+                cameraAngle,
+                onChangeCameraAngle,
+                topColor,
+                bottomColor,
+                isMobile } = this.props;
+        const menuClassNames = classNames({ 'is-open': isOpen });
 
-    const colorInputs = [];
-    ['top', 'bottom'].forEach((orientation, orientationIndex) => {
-        ['red', 'green', 'blue'].forEach((color, colorIndex) => {
-            colorInputs.push(
-                <ColorInput orientation={orientation}
-                            color={color}
-                            topColor={topColor}
-                            bottomColor={bottomColor}
-                            onChange={onChangeColor}
-                            isMobile={isMobile}
-                            key={`${orientationIndex}${colorIndex}`} />
-            );
+        const colorInputs = [];
+        ['top', 'bottom'].forEach((orientation, orientationIndex) => {
+            ['red', 'green', 'blue'].forEach((color, colorIndex) => {
+                colorInputs.push(
+                    <ColorInput orientation={orientation}
+                                color={color}
+                                topColor={topColor}
+                                bottomColor={bottomColor}
+                                onChange={onChangeColor}
+                                isMobile={isMobile}
+                                key={`${orientationIndex}${colorIndex}`} />
+                );
+            });
         });
-    });
 
-    return (
-        <div id="menu" className={menuClassNames}>
-            <div className="menu-open" onClick={onClickToggleMenu}>
-                menu
-            </div>
-            <div className="menu-contents">
-                <div className="menu-close" onClick={onClickToggleMenu}>
-                    X
+        return (
+            <div id="menu" className={menuClassNames}>
+                <div className="menu-open" onClick={onClickToggleMenu}>
+                    menu
                 </div>
+                <div className="menu-contents">
+                    <div className="menu-close" onClick={onClickToggleMenu}>
+                        X
+                    </div>
 
-                <h3 className="menu-title menu-background">orthofloat</h3>
+                    <h3 className="menu-title menu-background">orthofloat</h3>
 
-                <p className="menu-background">
-                    made by <a href="http://www.russrinzler.com" target="_blank">Russ Rinzler</a>.
-                    <br />
-                    click <a href="https://github.com/RussHR/orthofloat_src" target="_blank">here</a> for the code.
-                </p>
+                    <p className="menu-background">
+                        made by <a href="http://www.russrinzler.com" target="_blank">Russ Rinzler</a>.
+                        <br />
+                        click <a href="https://github.com/RussHR/orthofloat_src" target="_blank">here</a> for the code.
+                    </p>
 
-                <button onClick={() => onChangeColor(randomRGB(), randomRGB())}>
-                    toggle color
-                </button>
+                    <button onClick={() => onChangeColor(randomRGB(), randomRGB())}>
+                        toggle color
+                    </button>
 
-                <br /><br />
+                    <br /><br />
 
-                {colorInputs}
+                    {colorInputs}
 
-                <button onClick={() => onChangeCameraAngle(getNewCameraAngle(cameraAngle))}>
-                    rotate camera
-                </button>
+                    <button onClick={() => onChangeCameraAngle(getNewCameraAngle(cameraAngle))}>
+                        rotate camera
+                    </button>
 
-                <br /><br />
+                    <br /><br />
 
-                <label className="menu-background" htmlFor="camera-angle">camera angle </label>
-                <input type="range"
-                       id="camera-angle"
-                       min="0"
-                       max="359"
-                       name="camera-angle"
-                       value={cameraAngle}
-                       onChange={e => onChangeCameraAngle(parseInt(e.target.value))} />
+                    <label className="menu-background" htmlFor="camera-angle">camera angle </label>
+                    <input type="range"
+                           id="camera-angle"
+                           min="0"
+                           max="359"
+                           name="camera-angle"
+                           value={cameraAngle}
+                           onChange={e => onChangeCameraAngle(parseInt(e.target.value))} />
 
-                <br /><br />
+                    <br /><br />
 
-                <button onClick={onClickToggleStats}>
-                    toggle stats
-                </button>
+                    <button onClick={onClickToggleStats}>
+                        toggle stats
+                    </button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 Menu.propTypes = {
